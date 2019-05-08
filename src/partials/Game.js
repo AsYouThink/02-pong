b52bcf27a419863555560ab5426ea4824b7fd84c
DildarRana <dildar.rana@venora.no>
2019-05-07 20:44:11 -0700

    Update Game.js

--- src/partials/Game.js
+++ src/partials/Game.js
@@ -11,7 +11,7 @@ export default class Game {
     svg.setAttributeNS(null, "height", this.width);
     svg.setAttributeNS(null, "viewBox", `0 0 ${this.width} ${this.height}`);
     this.gameElement.appendChild(svg);
-    this.gameElement.appendChild(svg);
+    
 
 
 		// Other code goes here...
