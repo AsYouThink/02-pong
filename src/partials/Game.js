6a2be7ae9bcf1302da25a7d27305ad80ed3dd4dc
DildarRana <dildar.rana@venora.no>
2019-05-08 19:49:04 -0700

    Update Board.js, Game.js, Paddle.js and settings.js

--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,25 +1,31 @@
-import { SVG_NS } from '../settings';
+import { SVG_NS, PADDLE_WIDTH, PADDLE_HEIGHT, BOARD_GAP, KEYS} from '../settings';
 import Board from './Board';
+import Paddle from './Paddle';
 export default class Game {
   constructor(element, width, height) {
     this.element = element;
     this.width = width;
     this.height = height;
-    this.svg = document.createElementNS(SVG_NS, 'svg');
     this.gameElement = document.getElementById(this.element);
-    this.svg.setAttributeNS(null, "width", this.width);
-    this.svg.setAttributeNS(null, "height", this.height);
-    this.svg.setAttributeNS(null, "viewBox", `0 0 ${this.width} ${this.height}`);
-    this.gameElement.appendChild(this.svg);
     this.board = new Board(this.width, this.height);
-
-
+    const boardMid = (this.height - PADDLE_HEIGHT) /2;
+    this.paddle1 = new Paddle(this.height, PADDLE_WIDTH, PADDLE_HEIGHT, BOARD_GAP, boardMid, KEYS.p1up, KEYS.p1down);
+    const paddle2Gap = this.width - BOARD_GAP - PADDLE_WIDTH;
+    this.paddle2 = new Paddle(this.height, PADDLE_WIDTH, PADDLE_HEIGHT, paddle2Gap, boardMid, KEYS.p2up, KEYS.p2down);
+  
 		// Other code goes here...
   }
 
   render() {
     // More code goes here....
-    // this.gameElement.innerHTML = '';
-    this.board.render(this.svg);
+    this.gameElement.innerHTML = '';
+    let svg = document.createElementNS(SVG_NS, 'svg');
+    svg.setAttributeNS(null, "width", this.width);
+    svg.setAttributeNS(null, "height", this.height);
+    svg.setAttributeNS(null, "viewBox", `0 0 ${this.width} ${this.height}`);
+    this.gameElement.appendChild(svg);
+    this.board.render(svg);
+    this.paddle1.render(svg);
+    this.paddle2.render(svg);
   }
 }
